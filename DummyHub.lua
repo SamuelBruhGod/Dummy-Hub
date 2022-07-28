21e854de87997ba30b539996e29314a244b551cb
SamuelBruhGod <100874139+SamuelBruhGod@users.noreply.github.com>
2022-07-27 23:34:18 -0500

    Update DummyHub.lua

--- DummyHub.lua
+++ DummyHub.lua
@@ -1,12 +1,99 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local Loading = Instance.new("ScreenGui")
+local Background = Instance.new("Frame")
+local DummyHubv000 = Instance.new("TextLabel")
+local LoadingRing = Instance.new("ImageLabel")
+
+--Properties:
+
+Loading.Name = "Loading"
+Loading.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+Loading.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Loading.ResetOnSpawn = false
+
+Background.Name = "Background"
+Background.Parent = Loading
+Background.BackgroundColor3 = Color3.fromRGB(139, 139, 139)
+Background.Position = UDim2.new(0.388345927, 0, 0.323551059, 0)
+Background.Size = UDim2.new(0, 223, 0, 223)
+
+DummyHubv000.Name = "Dummy Hub v0.0.0"
+DummyHubv000.Parent = Background
+DummyHubv000.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+DummyHubv000.BackgroundTransparency = 1.000
+DummyHubv000.Position = UDim2.new(0.169780776, 0, 0.344291508, 0)
+DummyHubv000.Size = UDim2.new(0, 148, 0, 69)
+DummyHubv000.Font = Enum.Font.SourceSans
+DummyHubv000.Text = "Dummy Hub v0.0.0"
+DummyHubv000.TextColor3 = Color3.fromRGB(255, 255, 255)
+DummyHubv000.TextScaled = true
+DummyHubv000.TextSize = 14.000
+DummyHubv000.TextWrapped = true
+
+LoadingRing.Name = "LoadingRing"
+LoadingRing.Parent = Background
+LoadingRing.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+LoadingRing.BackgroundTransparency = 1.000
+LoadingRing.Position = UDim2.new(0.00448430516, 0, 0.00348437577, 0)
+LoadingRing.Size = UDim2.new(0, 222, 0, 223)
+LoadingRing.Image = "rbxassetid://4965945816"
+
+-- Scripts:
+
+local function NAXRYDD_fake_script() -- DummyHubv000.LocalScript 
+	local script = Instance.new('LocalScript', DummyHubv000)
+
+	wait(15)
+	script.Parent:TweenSize(UDim2.new(0,0,0,0),"Out","Sine",1)
+	
+end
+coroutine.wrap(NAXRYDD_fake_script)()
+local function GDRQLX_fake_script() -- LoadingRing.LocalScript 
+	local script = Instance.new('LocalScript', LoadingRing)
+
+	wait(15)
+	script.Parent:TweenSize(UDim2.new(0,0,0,0),"Out","Sine",1)
+	
+end
+coroutine.wrap(GDRQLX_fake_script)()
+local function UUDCCG_fake_script() -- Background.LocalScript 
+	local script = Instance.new('LocalScript', Background)
+
+	wait(15)
+	script.Parent:TweenSize(UDim2.new(0,0,0,0),"Out","Sine",1)
+end
+coroutine.wrap(UUDCCG_fake_script)()
+local function RQJJS_fake_script() -- Loading. 
+	local script = Instance.new('LocalScript', Loading)
+
+	local ReplicatedFirst = game:GetService("ReplicatedFirst")
+	local TweenService = game:GetService("TweenService")
+	local LoadingRing = script.Parent.Background.LoadingRing
+	
+	ReplicatedFirst:RemoveDefaultLoadingScreen()
+	
+	local tweenInfo = TweenInfo.new(4, Enum.EasingStyle.Linear, Enum.EasingDirection.In, -1)
+	local tween = TweenService:Create(LoadingRing, tweenInfo, {Rotation=360})
+	tween:Play()
+	
+	wait(5)
+	if not game:IsLoaded() then
+		game.Loaded:Wait()
+	end
+	
+	wait(10.8)
+	script.Parent:Destroy()
+end
+coroutine.wrap(RQJJS_fake_script)()
+
+
+wait(15.8)
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Dummy Hub", "LightTheme")
-local colors = {
-    SchemeColor = Color3.fromRGB(0,255,255),
-    Background = Color3.fromRGB(0, 0, 0),
-    Header = Color3.fromRGB(0, 0, 0),
-    TextColor = Color3.fromRGB(255,255,255),
-    ElementColor = Color3.fromRGB(20, 20, 20)
-}
 
 -- Main
 local Main = Window:NewTab("Main")
@@ -54,7 +141,7 @@ local Settings = Window:NewTab("Setting")
 local SettingsSection = Settings:NewSection("Settings")
 
 -- Setting --> Settings
-Section:NewKeybind("Open/Close", "Close it, Open it, do what ever", Enum.KeyCode.F, function()
+SettingsSection:NewKeybind("Open/Close", "Close it, Open it, do what ever", Enum.KeyCode.F, function()
 	Library:ToggleUI()
 end)
 
@@ -62,4 +149,4 @@ end)
 local Updates = Window:NewTab("Updates")
 local UpdatesSection = Updates:NewSection("UpdateLogs")
 
-UpdatesSection:NewLabel("7/27/2022: Release! :D")
+UpdatesSection:NewLabel("7/27/2022: v0.0.0 Release! :D")
